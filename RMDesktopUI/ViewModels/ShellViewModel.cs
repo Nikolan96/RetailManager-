cae1e5780ed1af2ee9e746a3537524c5ecb5a995
Nikola <nikolan96@gmail.com>
2019-05-04 16:06:47 +0200

    Adds Login Form
    Added LoginViewModel and LoginView.
    Added PasswordBoxHelper class and implemented it
    in Bootstrapper class.

--- RMDesktopUI/ViewModels/ShellViewModel.cs
+++ RMDesktopUI/ViewModels/ShellViewModel.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Caliburn.Micro;
 
 namespace RMDesktopUI.ViewModels
 {
-    public class ShellViewModel
+    // Conductor holds on to and activates only one item at a time.
+    public class ShellViewModel : Conductor<object>
     {
+        private LoginViewModel _loginVM;
+
+        // Uses constructor injection to pass in a new instance of LoginVM and activate it.
+        public ShellViewModel(LoginViewModel loginVM)
+        {
+            _loginVM = loginVM;
+            ActivateItem(_loginVM);
+        }
     }
 }
