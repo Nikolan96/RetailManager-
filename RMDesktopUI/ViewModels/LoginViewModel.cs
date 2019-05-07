2e730ab8ead14b2123be29d49f0e3808c4388aa5
Nikola <nikolan96@gmail.com>
2019-05-07 10:04:59 +0200

    Adds Error Handling
    Adds Error Handling for login form, to show error message.

--- RMDesktopUI/ViewModels/LoginViewModel.cs
+++ RMDesktopUI/ViewModels/LoginViewModel.cs
@@ -62,16 +62,49 @@ namespace RMDesktopUI.ViewModels
             }
         }
 
+        private bool _isErrorVisible;
+
+        public bool IsErrorVisible
+        {
+            get
+            {
+                bool output = false;
+                if (ErrorMessage?.Length > 0)
+                {
+                    output = true;
+                }
+                return output;
+            }
+          
+        }
+
+        private string _errorMessage;
+
+        public string ErrorMessage
+        {
+            get { return _errorMessage; }
+            set
+            {                
+                _errorMessage = value;
+                NotifyOfPropertyChange(() => IsErrorVisible);   
+                NotifyOfPropertyChange(() => ErrorMessage);
+            }
+        }
+
+
+
+
         // 
         public async Task LogIn()
         {
             try
             {
+                ErrorMessage = "";
                 var result = await _apiHelper.Authenticate(UserName, Password);
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.Message);
+                ErrorMessage = ex.Message;
             }
            
         }
