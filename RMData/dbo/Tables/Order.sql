6e38d96ee4bf533653bc1dd01c9930d46f965bfa
Nikola <nikolan96@gmail.com>
2019-07-19 12:26:05 +0200

    Adds Order and OrderItem tables Adds Orders,OrdersForm,OrderDetails and EditOrderItem views and viewModels

---
+++ RMData/dbo/Tables/Order.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Order]
+(
+    [ID] nvarchar(150) NOT NULL PRIMARY KEY, 
+    [CreatedDate] DATETIME2 NOT NULL DEFAULT getutcdate(), 
+	[ShopID] int NOT NULL foreign key references Shop(ID),
+	[IsApproved] Bit NOT NULL
+)
---
+++ RMData/dbo/Tables/OrderItem.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[OrderItem]
+(
+    [ID] INT NOT NULL PRIMARY KEY IDENTITY, 
+	[OrderID] nvarchar(150) NOT NULL FOREIGN KEY REFERENCES [ORDER](ID),
+	[ProductName] NVARCHAR(100) NOT NULL, 
+	[Quantity] int NOT NULL
+)
